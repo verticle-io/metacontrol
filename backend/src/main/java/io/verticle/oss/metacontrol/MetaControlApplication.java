b5280824b56ebe5841e9923e0a3cd37db044416b
Sam Jones <sam@verticle.io>
2020-11-05 20:05:25 +0100

    initial project skeleton

---
+++ backend/src/main/java/io/verticle/oss/metacontrol/MetaControlApplication.java
@@ -0,0 +1,13 @@
+package io.verticle.oss.metacontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaControlApplication.class, args);
+	}
+
+}
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  brokerUrl: "wss://metacontrol.herokuapp.com/ws",
+  gtmId: "GTM-MHG9BFX"
+};
